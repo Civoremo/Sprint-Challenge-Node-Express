5aa87452ca7a0ab0e2019d25d9706f08f308687a
Civoremo <omerovic_n@hotmail.com>
2019-01-12 10:34:49 -0500

    heroku changes

--- api/server.js
+++ api/server.js
@@ -4,9 +4,6 @@ const configMiddleware = require('../config/middleware.js');
 const actionsRouter = require('../action/actionsRouter.js');
 const projectsRouter = require('../project/projectsRouter.js');
 
-const path = require('path'); // heroku
-server.use(express.static(path.join(__dirname, 'client/build'))); // heroku
-
 const server = express();
 configMiddleware(server);
 
@@ -14,14 +11,8 @@ server.use( actionsRouter);
 server.use( projectsRouter);
 
 
-// server.get('/', (req, res) => {
-//     res.send('server connected');
-// });
-
-
-server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+server.get('/', (req, res) => {
+    res.send('server connected');
 });
-
 
 module.exports = server;
