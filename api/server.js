6cda51aff220ef83c7dcda0524958bdcb0394d3a
Civoremo <omerovic_n@hotmail.com>
2019-01-12 10:45:57 -0500

    another heroku fix attempt

--- api/server.js
+++ api/server.js
@@ -4,15 +4,22 @@ const configMiddleware = require('../config/middleware.js');
 const actionsRouter = require('../action/actionsRouter.js');
 const projectsRouter = require('../project/projectsRouter.js');
 
+const path = require('path');
+
 const server = express();
 configMiddleware(server);
 
 server.use( actionsRouter);
 server.use( projectsRouter);
 
+server.use(express.static(path.join(__dirname, 'client/build')));
 
-server.get('/', (req, res) => {
-    res.send('server connected');
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+// server.get('/', (req, res) => {
+//     res.send('server connected');
+// });
 
 module.exports = server;
